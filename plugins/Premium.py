eac05d18e28c92a5af1b440d284f3c3edf5fb06f
botwalain <167398125+botwalain@users.noreply.github.com>
2025-03-01 23:54:45 +0530

    Update Premium.py

--- plugins/Premium.py
+++ plugins/Premium.py
@@ -159,5 +159,5 @@ async def plan(client, message):
         ],[            
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ ⇋', callback_data='start')
     ]]
-    await message.reply_photo(photo="https://envs.sh/Wdj.jpg", caption=script.PREMIUM_TEXT.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(btn))
+    await message.reply_photo(photo="https://envs.sh/bdt.jpg", caption=script.PREMIUM_TEXT.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(btn))
     

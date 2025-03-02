cf866672641cb9addafc529d2f92cdb01f0cf7ef
botwalain <167398125+botwalain@users.noreply.github.com>
2025-03-02 07:41:03 +0530

    Update ask.py

--- plugins/Lucy/ask.py
+++ plugins/Lucy/ask.py
@@ -17,7 +17,7 @@ async def gpt_handler(client: Client, message: Message):
         name = message.from_user.first_name or "User"
 
         if len(message.command) < 2:
-            await message.reply_text(f"<b>Hello {name}, I am Lucy. How can I help you today?</b>")
+            await message.reply_text(f"<b>Hello {name}, How can I help you today?</b>")
             return
 
         query = message.text.split(' ', 1)[1]

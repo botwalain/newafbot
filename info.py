2a9117b94e4f793e78f55357b285deba537b31ef
botwalain <167398125+botwalain@users.noreply.github.com>
2025-03-02 08:00:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -182,9 +182,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://occupational-isabelita-movbotbyibm-684fdecb.koyeb.app/".format(FQDN)
 else:
-    URL = "http://{}/".format(FQDN)
+    URL = "http://occupational-isabelita-movbotbyibm-684fdecb.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration

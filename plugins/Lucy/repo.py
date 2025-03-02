c90db395726b529a957ee2ee468a74cccd627394
botwalain <167398125+botwalain@users.noreply.github.com>
2025-03-02 07:39:22 +0530

    Update repo.py

--- plugins/Lucy/repo.py
+++ plugins/Lucy/repo.py
@@ -30,7 +30,7 @@ async def git(Kashmira, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @Codeflix_Bots
+<b>Powered by :</b> @IBMBotSupport
 """
         if qw.get("description"):
             txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
